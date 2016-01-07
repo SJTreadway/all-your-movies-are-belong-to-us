6395552c43e62d56a6415d71283fe8cc52911176
SJTreadway <treadway08@gmail.com>
2016-01-06 20:23:34 -0600

    changes

--- public/views/cart/cartCtrl.js
+++ public/views/cart/cartCtrl.js
@@ -2,10 +2,6 @@
 	angular.module('storeApp').controller('cartCtrl', function($scope, mainService) {
 
 		$scope.emptyCart = function() {
-			if ($scope.$parent.user.cart.items.length === 0) {
-				alert('Please add items to your cart before checking out.');
-				return;
-			}
 			mainService.emptyCart($scope.$parent.user._id).then(function(res) {
 				$scope.$parent.getItems();
 				alert("Thank you for your purchase!");
